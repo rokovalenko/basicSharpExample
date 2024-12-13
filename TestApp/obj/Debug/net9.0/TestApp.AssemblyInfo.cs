ca05ed02da23dc68df918524972ebea243ba6efe
rokovalenko <rokovalenko@outlook.com>
2024-12-13 18:45:48 +0100

    second

--- TestApp/obj/Debug/net9.0/TestApp.AssemblyInfo.cs
+++ TestApp/obj/Debug/net9.0/TestApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88360a79864bbafc0c872fe3bedb0d9c24224f4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
